41420c2e13799c55e90236aa6b7ddd6691ebd72d
Kavia AI <production@kavia.ai>
2025-06-11 05:20:01 +0000

    CheckPoint

--- secureauth_with_supabase/.ve/webpack.config.js
+++ secureauth_with_supabase/.ve/webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, '../src/index.js'),
@@ -10,35 +9,63 @@ module.exports = {
     clean: true,
   },
   mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        include: path.resolve(__dirname, '../src'),
-        use: ['babel-loader'],
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      }
-    ],
+  devtool: 'cheap-module-source-map',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, '../public'),
+    },
+    historyApiFallback: true,
+    hot: true,
+    compress: true,
+    port: 3000,
+    host: '0.0.0.0',
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '../public/index.html'),
-      templateParameters: {
-        PUBLIC_URL: ''
-      }
-    }),
-  ],
   resolve: {
     extensions: ['.js', '.jsx'],
   },
-  devServer: {
-    allowedHosts: 'all',
-    host: '0.0.0.0',
-    historyApiFallback: true,
-    port: 3000,
-    hot: true
-  }
+  module: {
+    rules: [
+      // Babel loader for .js and .jsx
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            // Make sure both presets are included, particularly preset-react
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ],
+            // You can also have plugins from package.json if needed
+          },
+        },
+      },
+      // Styles: CSS loader and style-loader
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      // Asset/resource (e.g. images, fonts)
+      {
+        test: /\.(png|jpe?g|gif|svg|eot|ttf|woff2?)$/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    // Only add HtmlWebpackPlugin if it exists
+    (() => {
+      try {
+        const HtmlWebpackPlugin = require('html-webpack-plugin');
+        return new HtmlWebpackPlugin({
+          template: path.resolve(__dirname, '../public/index.html'),
+          filename: 'index.html',
+          inject: true,
+        });
+      } catch (err) {
+        return undefined;
+      }
+    })(),
+  ].filter(Boolean),
 };
